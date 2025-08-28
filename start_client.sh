37bb47b73ca58249c1ad396b051327f1b7c7f44d
Bruno Querino <bruno.querino@skf.com>
2025-08-28 00:51:00 -0300

    chore: removing unnused files

--- start_client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Inicia cliente interativo apontando para node1
-read -p "Digite o nó (node1/node2/node3): " NODE
-read -p "Digite a porta (8081/8082/8083): " PORTA
-start /B cmd /C "go run cmd/client.go localhost $PORTA > client_$NODE.log 2>&1"
--- start_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Inicia node1
-go run cmd/server.go node1 8081 > node1.log 2>&1
-# Inicia node2
-go run cmd/server.go node2 8082 > node2.log 2>&1
-# Inicia node3
-go run cmd/server.go node3 8083 > node3.log 2>&1
